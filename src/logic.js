cc229fc596b233f6e83bda882df6ec9f97ae8795
Compile Dev <dev@compile.local>
2026-05-01 22:48:38 +0200

    feat(ux): replace shield with padlock icon for immune badge

--- src/logic.js
+++ src/logic.js
@@ -1147,7 +1147,7 @@ function renderStack(line, target) {
             if (mods.preventFlip || mods.preventShift || mods.preventEliminate) {
                 const badge = document.createElement('span');
                 badge.className = 'immune-badge';
-                badge.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 12" fill="currentColor"><path d="M5 0L0 2.2v4.2C0 9.5 2.5 12 5 12s5-2.5 5-5.6V2.2L5 0z"/></svg>`;
+                badge.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18 8h-1V6A5 5 0 0 0 7 6v2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zm-6 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm3.1-9H8.9V6a3.1 3.1 0 0 1 6.2 0v2z"/></svg>`;
                 domCard.appendChild(badge);
             }
         }
